b73e1d4b214aa2621ff54a0666b409f541d9c8e8
jcmonin83 <jose.monin@gmail.com>
2022-04-08 12:26:01 -0500

    Initial commit

---
+++ practica3/Bebidas+CoreDataClass.swift
@@ -0,0 +1,26 @@
+//
+//  Bebidas+CoreDataClass.swift
+//  practica3
+//
+//  Created by DISMOV on 07/04/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Bebidas)
+public class Bebidas: NSManagedObject {
+
+    func inicializaCon (_ dict: [String:String]){
+        let nombre = (dict["name"]) ?? ""
+        let ingredientes = (dict["ingredients"]) ?? ""
+        let instructions = (dict["directions"]) ?? ""
+        let image = (dict["image"]) ?? "drinks"
+        self.name = nombre
+        self.ingredients = ingredientes
+        self.directions = instructions
+        self.image = image
+    }
+    
+}
